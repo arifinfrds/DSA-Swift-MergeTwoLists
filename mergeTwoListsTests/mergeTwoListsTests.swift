2a4924a74a85f8c1ec7c01625e5d47e5f299270d
arifinfrds <arifinfrds.engineer@gmail.com>
2023-01-13 10:26:37 +0700

    `mergeTwoLists` can return nil on both list nil

--- mergeTwoListsTests/mergeTwoListsTests.swift
+++ mergeTwoListsTests/mergeTwoListsTests.swift
@@ -8,29 +8,45 @@
 import XCTest
 @testable import mergeTwoLists
 
+// Definition for singly-linked list.
+public class ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs.val == rhs.val && lhs.next == rhs.next
+    }
+    
+    public var val: Int
+    public var next: ListNode?
+    
+    public init() {
+        self.val = 0
+        self.next = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+    
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
+
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        return nil
+    }
+}
+
 final class mergeTwoListsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_mergeTwoLists_returnsNilOnBothListNil() {
+        let sut = Solution()
+        
+        let result = sut.mergeTwoLists(nil, nil)
+        
+        XCTAssertEqual(result, nil)
     }
 
 }
